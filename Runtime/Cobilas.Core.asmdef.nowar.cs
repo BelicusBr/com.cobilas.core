16a8fb9b97f8153bc91e07aef04b1170f05c749c
BelicusBr <x70r@outlook.com>
2023-08-29 19:52:42 -0300

    #Update-vs2.0.0-ch1

---
+++ Runtime/Cobilas.Core.asmdef.nowar.cs
@@ -0,0 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("", "IDE1006", Scope = "module")]
+[assembly: SuppressMessage("", "IDE0028", Scope = "module")]
+[assembly: SuppressMessage("", "IDE0017", Scope = "module")]
+[assembly: SuppressMessage("", "CS0108", Scope = "module")]
+[assembly: SuppressMessage("", "CS0672", Scope = "module")]
+[assembly: SuppressMessage("", "CS0618", Scope = "module")]
